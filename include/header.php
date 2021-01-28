1c73918e82bdcad710b082e64ddb3b2877c6301d
ujgsp <ujangsp11@gmail.com>
2021-01-28 17:05:26 +0700

    rencana menambahkan dashboard dan men getData bln

--- include/header.php
+++ include/header.php
@@ -37,14 +37,21 @@ session_start();
     <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
       <span class="navbar-toggler-icon"></span>
     </button>
+    
     <div class="collapse navbar-collapse" id="navbarResponsive">
+
+    <form action="<?php echo $_SERVER['PHP_SELF'] ?>" method="get">
       <ul class="navbar-nav navbar-sidenav" id="exampleAccordion">
         <li class="nav-item" data-toggle="tooltip" data-placement="right" title="Dashboard">
           <a class="nav-link" href="dashboard.php">
+
             <i class="fa fa-fw fa-dashboard"></i>
             <span class="nav-link-text">Dashboard</span>
           </a>
+    </form>
+
         </li>
+        
         <?php
         if($_SESSION['level'] == "admin")
         {
